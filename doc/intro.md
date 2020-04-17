f5aa8cb60970bb87dd79446725f13cfda07ad1e8
Tiv0w <t.theomeyer@gmail.com>
2020-04-17 23:53:27 +0200

    :tada: Initial commit (lein autogenerated)

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to projet-gaelle-api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/projet_gaelle_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns projet-gaelle-api.core-test
+  (:require [clojure.test :refer :all]
+            [projet-gaelle-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
