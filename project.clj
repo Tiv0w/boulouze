3bbf7b7815af0de559b44454d5093c0eff331fd6
Tiv0w <t.theomeyer@gmail.com>
2020-04-18 00:08:18 +0200

    :heavy_plus_sign: add liberator, compojure, ring dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [liberator "0.15.3"]
+                 [compojure "1.6.1"]
+                 [ring/ring-core "1.8.0"]]
+  :ring {:handler projet-gaelle-api.core/handler}
   :repl-options {:init-ns projet-gaelle-api.core})
