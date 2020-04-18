c439aeb335deb92645491234dd1c4de3720b9bb9
Tiv0w <t.theomeyer@gmail.com>
2020-04-19 00:16:42 +0200

    :sparkles: working on the basics (routing and all...)

--- src/projet_gaelle_api/core.clj
+++ src/projet_gaelle_api/core.clj
@@ -1,6 +1,43 @@
-(ns projet-gaelle-api.core)
+(ns projet-gaelle-api.core
+  (:require [compojure.core :refer [defroutes GET]]
+            [liberator.core :refer [defresource]]
+            [liberator.core :as l]
+            [ring.middleware.params :refer [wrap-params]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defresource oh-hi [name]
+  :available-media-types ["text/html"]
+  :handle-ok (fn [_]
+               (->> name
+                    clojure.string/upper-case
+                    (format "<html><h1>OH HI %s!</h1></html>"))))
+
+(defresource wtf []
+  :available-media-types ["text/html"]
+  :malformed? (fn [ctx]
+                (nil? (get-in ctx [:request :params "name"])))
+  :handle-ok (fn [ctx]
+               (->> (get-in ctx [:request :params "name"])
+                    clojure.string/upper-case
+                    (format "<html><h1>WHAT THE F*CK %s?!</h1></html>")))
+  :handle-malformed "We could not process your request, it seems like the parameter \"name\" was omitted.")
+
+(defresource welcome []
+  :available-media-types ["text/html"]
+  :handle-ok (clojure.java.io/file "resources/index.html"))
+
+(defresource favicon []
+  :available-media-types ["image/vnd.microsoft.icon"]
+  :exists? (fn [ctx]
+             (.exists (clojure.java.io/file "resources/favicon.ico")))
+  :handle-ok (new java.io.File "resources/favicon.ico"))
+
+
+(defroutes app-routes
+  (GET "/" [] (welcome))
+  (GET "/favicon.ico" [] (favicon))
+  (GET "/hi/:name" [name] (oh-hi name))
+  (GET "/wtf" [] (wtf)))
+
+(def handler
+  (-> app-routes
+      wrap-params))
