26eb3d62fa7aea8c80d3e9730051269cf3daf15a
Tiv0w <t.theomeyer@gmail.com>
2020-04-20 09:04:43 +0200

    :construction: trying some things with static resources

--- src/projet_gaelle_api/core.clj
+++ src/projet_gaelle_api/core.clj
@@ -1,8 +1,12 @@
 (ns projet-gaelle-api.core
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [liberator.core :refer [defresource]]
             [liberator.core :as l]
-            [ring.middleware.params :refer [wrap-params]]))
+            [projet-gaelle-api.middleware :as m]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]))
 
 (defresource oh-hi [name]
   :available-media-types ["text/html"]
@@ -25,19 +29,40 @@
   :available-media-types ["text/html"]
   :handle-ok (clojure.java.io/file "resources/index.html"))
 
-(defresource favicon []
-  :available-media-types ["image/vnd.microsoft.icon"]
-  :exists? (fn [ctx]
-             (.exists (clojure.java.io/file "resources/favicon.ico")))
-  :handle-ok (new java.io.File "resources/favicon.ico"))
+(defresource yes-sir []
+  :available-media-types ["text/plain"]
+  :handle-ok "yes sir")
+
+(defresource upload-file []
+  :available-media-types ["text/plain"]
+  :allowed-methods [:post]
+  :malformed? (fn [ctx]
+                (nil? (get-in ctx [:request :params "file"])))
+  :handle-created (fn [ctx]
+                    (let [fichier (get-in ctx [:request :params "file"])]
+                      (println fichier)
+                      (println (get-in fichier [:tempfile]))))
+  :handle-method-not-allowed "Method should be a POST")
+
+
+;; (defresource favicon []
+;;   :available-media-types ["image/vnd.microsoft.icon"]
+;;   :exists? (fn [ctx]
+;;              (.exists (clojure.java.io/file "resources/favicon.ico")))
+;;   :handle-ok (new java.io.File "resources/favicon.ico"))
 
 
 (defroutes app-routes
   (GET "/" [] (welcome))
-  (GET "/favicon.ico" [] (favicon))
+  ;; (GET "/favicon.ico" [] (favicon))
   (GET "/hi/:name" [name] (oh-hi name))
-  (GET "/wtf" [] (wtf)))
+  (GET "/wtf" [] (wtf))
+  (GET "/yes" [] (yes-sir))
+  (POST "/upload-file" [] (upload-file)))
 
 (def handler
   (-> app-routes
-      wrap-params))
+      m/remove-trailing-slashes
+      (wrap-resource "public")
+      wrap-params
+      wrap-multipart-params))
---
+++ src/projet_gaelle_api/middleware.clj
@@ -0,0 +1,13 @@
+(ns projet-gaelle-api.middleware)
+
+(defn remove-trailing-slashes [handler]
+  (fn [req]
+    (let [uri (:uri req)
+          not-root? (not= uri "/")
+          ends-with-slash? (.endsWith ^String uri "/")
+          subs-uri (subs uri 0 (dec (count uri)))
+          fixed-uri (if (and not-root? ends-with-slash?)
+                      subs-uri
+                      uri)
+          fixed-req (assoc req :uri fixed-uri)]
+      (handler fixed-req))))
