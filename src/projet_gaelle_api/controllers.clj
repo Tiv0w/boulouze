7062a1212fa3324e51b6d643a6ecfaac3be308e1
Tiv0w <t.theomeyer@gmail.com>
2020-07-27 19:23:56 +0200

    :recycle: big refacto

--- src/projet_gaelle_api/controllers.clj
+++ src/projet_gaelle_api/controllers.clj
@@ -1,24 +1,25 @@
-(ns projet-gaelle-api.resources
-  (:require [projet-gaelle-api.services.file :as file-service]
-            [projet-gaelle-api.services.db :as db-service]
-            [liberator.core :refer [defresource]]))
+(ns projet-gaelle-api.controllers
+  (:require
+   [projet-gaelle-api.services.file :as file-service]
+   [projet-gaelle-api.services.db :as db-service]
+   [liberator.core :as liberator]))
 
-(defresource welcome []
+(liberator/defresource welcome []
   :available-media-types ["text/html"]
   :handle-ok (clojure.java.io/file "resources/public/index.html"))
 
-(defresource yes-sir []
+(liberator/defresource yes-sir []
   :available-media-types ["text/plain"]
   :handle-ok "yes sir")
 
-(defresource oh-hi [name]
+(liberator/defresource oh-hi [name]
   :available-media-types ["text/html"]
   :handle-ok (fn [_]
                (->> name
                     clojure.string/upper-case
                     (format "<html><h1>OH HI %s!</h1></html>"))))
 
-(defresource wtf []
+(liberator/defresource wtf []
   :available-media-types ["text/html"]
   :malformed? (fn [ctx]
                 (nil? (get-in ctx [:request :params "name"])))
@@ -28,7 +29,7 @@
                     (format "<html><h1>WHAT THE F*CK %s?!</h1></html>")))
   :handle-malformed "We could not process your request, it seems like the parameter \"name\" was omitted.")
 
-(defresource upload-file []
+(liberator/defresource upload-file []
   :available-media-types ["text/plain"]
   :allowed-methods [:post]
   :malformed? (fn [ctx]
@@ -41,13 +42,31 @@
              (file-service/save-param-to-dir file "pics/" true)))
   :handle-method-not-allowed "Method should be a POST")
 
-;; (defresource favicon []
+(liberator/defresource list-files []
+  :available-media-types ["text/plain"]
+  :allowed-methods [:get]
+  :handle-ok (file-service/list-files)
+  :handle-method-not-allowed "Method should be a POST")
+
+(liberator/defresource list []
+  :available-media-types ["text/html"]
+  :handle-ok (clojure.java.io/file "resources/public/list-images.html"))
+
+(liberator/defresource get-image []
+  :available-media-types ["image/jpeg" "image/png"]
+  :handle-ok (fn [ctx]
+               (let [path (get-in ctx [:request :params "path"])
+                     file (clojure.java.io/file path)]
+                 (println path file)
+                 file)))
+
+;; (liberator/defresource favicon []
 ;;   :available-media-types ["image/vnd.microsoft.icon"]
 ;;   :exists? (fn [ctx]
 ;;              (.exists (clojure.java.io/file "resources/favicon.ico")))
 ;;   :handle-ok (new java.io.File "resources/favicon.ico"))
 
-(defresource not-found []
+(liberator/defresource not-found []
   :available-media-types ["text/html"]
   :exists? false
   :handle-not-found (clojure.java.io/file "resources/public/404.html"))
--- src/projet_gaelle_api/core.clj
+++ src/projet_gaelle_api/core.clj
@@ -1,12 +1,12 @@
 (ns projet-gaelle-api.core
-  (:require [projet-gaelle-api.middleware :as middleware]
-            [projet-gaelle-api.resources :as resources]
-            [compojure.core :refer [defroutes GET POST ANY]]
+  (:require [compojure.core :refer [ANY defroutes GET POST]]
             [jumblerg.middleware.cors :refer [wrap-cors]]
-            [ring.middleware.params :refer [wrap-params]]
+            [projet-gaelle-api.controllers :as resources]
+            [projet-gaelle-api.middleware :as middleware]
             [ring.middleware.keyword-params :refer [wrap-keyword-params]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.multipart-params :refer [wrap-multipart-params]]))
+            [ring.middleware.multipart-params :refer [wrap-multipart-params]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.resource :refer [wrap-resource]]))
 
 (defroutes app-routes
   (GET "/" [] (resources/welcome))
@@ -14,6 +14,9 @@
   (GET "/wtf" [] (resources/wtf))
   (GET "/yes" [] (resources/yes-sir))
   (ANY "/upload-file" [] (resources/upload-file))
+  (GET "/list-files" [] (resources/list-files))
+  (GET "/list" [] (resources/list))
+  (GET "/get-image" [] (resources/get-image))
   ;; (GET "/favicon.ico" [] (favicon))
   (ANY "*" [] (resources/not-found))
   )
