1aa78d01717fbcc426e2df386e947aab7b722744
MargotDumoulin <margot.dumoulin18@gmail.com>
2020-10-10 16:38:11 +0200

    :sparkles: create gallery

---
+++ boulouze-app/src/components/Gallery.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import axios from 'axios';
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IonGrid, IonImg, IonRow, IonCol, withIonLifeCycle, IonCardContent, IonCard } from '@ionic/react';
+
+type Props = {};
+type State = { imagesList: string[] }
+class Gallery extends React.Component<Props, State> {
+    constructor(props: any) {
+      super(props);
+      this.state = {
+        imagesList: []
+      };
+
+      this.getImagesList = this.getImagesList.bind(this);
+    }
+
+    componentDidMount() {
+      this.getImagesList();
+    }
+
+    ionViewWillEnter() {
+      this.getImagesList();
+      console.log('ion view lalal');
+    }
+
+    getImagesList() {
+      axios.get('http://localhost:3000/list-files')
+        .then((response) => {
+          this.setState({
+            imagesList: response.data.map((file: any) => 'http://localhost:3000/get-image?path=' + file.path)
+          })
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+    }
+  
+    render() {
+      return (
+          <IonGrid>
+            <IonRow>
+              { this.state.imagesList.map((img: string, index: number) => 
+                  
+                      <IonCol size="2">
+                          <IonCard key={index}>
+                                  <IonCardContent>
+                                      <IonImg src={img} />
+                                  </IonCardContent>
+                          </IonCard>
+                      </IonCol>
+              )}
+              </IonRow>
+        </IonGrid>   
+      );
+    }
+}
+  
+export default withIonLifeCycle(Gallery);
