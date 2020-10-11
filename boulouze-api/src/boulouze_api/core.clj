a854d869c64079c550b336a7ee93c80f78cf2d9e
Tiv0w <t.theomeyer@gmail.com>
2020-10-12 00:17:53 +0200

    :sparkles: Add wrap-json-response middleware

--- boulouze-api/src/boulouze_api/core.clj
+++ boulouze-api/src/boulouze_api/core.clj
@@ -4,7 +4,7 @@
    [jumblerg.middleware.cors :refer [wrap-cors]]
    [boulouze-api.controllers :as resources]
    [boulouze-api.middleware :as middleware]
-   [ring.middleware.json :refer [wrap-json-body]]
+   [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
    [ring.middleware.multipart-params :refer [wrap-multipart-params]]
    [ring.middleware.params :refer [wrap-params]]
    [ring.middleware.reload :refer [wrap-reload]]
@@ -31,5 +31,6 @@
       (wrap-cors identity)
       wrap-params
       (wrap-json-body {:keywords? true})
+      wrap-json-response
       wrap-reload ;; FIXME: remove this in production!!!
       wrap-multipart-params))
