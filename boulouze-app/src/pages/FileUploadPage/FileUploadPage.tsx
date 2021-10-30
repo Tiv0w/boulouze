3704b1f676ef3a8f1e19b35f5cca730c11814aa4
Tiv0w <t.theomeyer@gmail.com>
2021-10-31 00:00:55 +0200

    Permit closing the toasts

--- boulouze-app/src/pages/FileUploadPage/FileUploadPage.tsx
+++ boulouze-app/src/pages/FileUploadPage/FileUploadPage.tsx
@@ -40,6 +40,7 @@ const FileUploadPage: React.FC<Props> = () => {
             message: 'Votre produit a bien été enregistré !',
             duration: 3000,
             color: 'success',
+            buttons: [{ icon: 'close', role: 'cancel' }],
         });
         history.push('/tab2');
     };
@@ -50,6 +51,7 @@ const FileUploadPage: React.FC<Props> = () => {
             message: 'Une erreur s\'est produite lors de l\'enregistrement.',
             duration: 6000,
             color: 'danger',
+            buttons: [{ icon: 'close', role: 'cancel' }],
         });
     };
 
